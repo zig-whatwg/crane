2923ece0655bfcb90797f8219aa3a5c1fe18ee6f
Brian Cardarella <bcardarella@gmail.com>
2025-11-14 04:08:54 -0500

    Migrate CustomElementRegistry.zig from ArrayList to infra.List
    - Change scoped_document_set from ArrayList to infra.List
    - Add infra import
    - 2 ArrayList API usages converted to infra.List
    
    Part of systematic ArrayList → infra.List migration.

--- webidl/src/dom/CustomElementRegistry.zig
+++ webidl/src/dom/CustomElementRegistry.zig
@@ -1,18 +1,19 @@
 const std = @import("std");
 const webidl = @import("webidl");
+const infra = @import("infra");
 
 const Allocator = std.mem.Allocator;
 
 pub const CustomElementRegistry = webidl.interface(struct {
     allocator: Allocator,
     is_scoped: bool,
-    scoped_document_set: std.ArrayList(*anyopaque),
+    scoped_document_set: infra.List(*anyopaque),
 
     pub fn init(allocator: Allocator, is_scoped: bool) !CustomElementRegistry {
         return .{
             .allocator = allocator,
             .is_scoped = is_scoped,
-            .scoped_document_set = std.ArrayList(*anyopaque).init(allocator),
+            .scoped_document_set = infra.List(*anyopaque).init(allocator),
         };
     }
 
