275df6b13d86607ef40d87788109debc8eb8bdc4
Brian Cardarella <bcardarella@gmail.com>
2025-11-14 06:39:26 -0500

    refactor(main): migrate main.zig test to infra.List

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const whatwg = @import("whatwg");
+const infra = @import("infra");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
@@ -9,10 +10,10 @@ pub fn main() !void {
 
 test "simple test" {
     const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var list = infra.List(i32).init(gpa);
+    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop().?);
 }
 
 test "fuzz example" {
